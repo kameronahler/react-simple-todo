c85bdc992346710b874af34615b76f706804e036
Kameron Ahler <kamahler@gmail.com>
2020-08-20 17:22:04 -0600

    shhhh eslint shhhh

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import root from 'react-shadow'
 import Form from '../Form/Form'
 import List from '../List/List'
