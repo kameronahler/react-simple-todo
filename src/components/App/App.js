98cdf35d807036a0aefddd479ca596f9fb9cc555
Kameron Ahler <kamahler@gmail.com>
2020-08-20 12:59:54 -0600

    use uuid, structure todo item props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
 import root from 'react-shadow'
+import { v4 as uuid } from 'uuid'
 
 export default function App() {
+  const [totalItems, setTotalItems] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  const updateInput = (e) => {
+    setInputValue(e.currentTarget.value)
+  }
+
   const formSubmit = (e) => {
     e.preventDefault()
+
+    setTotalItems([
+      {
+        id: uuid(),
+        label: inputValue,
+        completed: false,
+      },
+      ...totalItems,
+    ])
   }
 
-  const [stateItems, setStateItems] = useState([])
-  const [stateInputValue, setStateInputValue] = useState('')
-
-  const updateInputInput = (e) => {
-    setStateInputValue(e.currentTarget.value)
-  }
+  // debug
+  console.log(totalItems)
 
   return (
     <root.div>
@@ -19,10 +32,10 @@ export default function App() {
         <label htmlFor='add-item'>Add Item</label>
         <input
           id='add-item'
-          onChange={updateInputInput}
+          onChange={updateInput}
           placeholder='New item'
           type='text'
-          value={stateInputValue}
+          value={inputValue}
         />
         <input type='submit' value='Add' />
       </form>
